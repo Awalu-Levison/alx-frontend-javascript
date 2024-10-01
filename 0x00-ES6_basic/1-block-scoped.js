81f7acbfedfc80e5b6fc08b80a802fe245532ea1
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-01 10:41:29 +0000

    Updated task 1 file

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
-var task = false;
-var task2 = true;
-
 export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
     let task = true;
     let task2 = false;
   }
-
   return [task, task2];
 }
