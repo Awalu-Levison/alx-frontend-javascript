3b335eef1bed6a84e1b6d3103c281f3c63d4cf17
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-03 00:30:11 +0000

    10. For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   const my_arr = [];
   for (const idx of array) {
     const value = idx;
-    arr.push(appendString + value);
+    my_arr.push(appendString + value);
   }
 
   return my_arr;
