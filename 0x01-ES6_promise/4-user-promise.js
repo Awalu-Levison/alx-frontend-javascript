a2301ecbec71fb42e96823d71912ecd219ed1916
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-04 00:57:54 +0000

    4. Simple promise

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,7 @@
+export default function signUpUser(firstName, lastName) {
+  /*Signup user and return data if successful*/
+  return promise.resolve({
+    firstName: '${firstName}',
+    lastName: '${lastname}',
+  });
+}
