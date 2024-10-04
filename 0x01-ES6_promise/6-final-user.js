71e10f0ea41e2936ced2947d8bb554b237a76273
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-04 01:37:40 +0000

    6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,10 +6,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((values) => {
-    const arr = [];
+    const myarr = [];
     for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
+      myarr.push({ status: item.status, value: item.value || item.reason });
     }
-    return arr;
+    return myarr;
   });
 }
