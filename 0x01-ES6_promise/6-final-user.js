11fe2cfea02ac09eb02e262827f84793c3fcd3f2
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-06 15:54:26 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -9,9 +9,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     const output = results.map((result) => {
       if (result.status === 'fulfilled') {
         return { status: result.status, value: result.value };
-      } else {
-        return { status: result.status, reason: result.reason };
       }
+      return { status: result.status, reason: result.reason };
     });
     return output;
   });
