52adffb06802df4ae850af5202c2c6f724689a36
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-09 01:45:49 +0000

    3. Reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(students) {
-  /*iterate through students list, a: total of students, b: current student object*/
+  /* iterate through students list, a: total of students, b: current student object */
   return students.reduce((a, b) => a + b.id, 0);
 }
