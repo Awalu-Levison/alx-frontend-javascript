ce696bf01668bf1977f4b0a064db887dfd2d635e
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-12 03:35:45 +0000

    10. Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo;
+  let user;
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (error) {
+    photo = null;
+    user = null;
+  }
+  /* Catching errors */
+  return { photo, user };
+}
